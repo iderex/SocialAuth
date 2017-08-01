ef2964b468a028faabcb98cb065075b280dcdb17
Nils Lehnen <30603423+iderex@users.noreply.github.com>
2017-08-02 00:17:31 +0200

    Add files via upload

---
+++ js/fb.js
@@ -0,0 +1,95 @@
+window.fbAsyncInit = function() {
+    FB.init({
+      appId      : '403273760070518',
+      cookie     : true,
+      xfbml      : true,
+      version    : 'v2.8'
+    });
+    
+    FB.getLoginStatus(function(response) {
+        statusChangeCallback(response);
+    }); 
+};
+(function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+}
+(document, 'script', 'facebook-jssdk'));
+
+function statusChangeCallback(response){
+  if(respnse.status === 'connected'){
+    console.log('Logged in and authenticated');
+    setElements(true);
+    testAPI();
+  } else {
+    console.log('Not authenticated');
+    setElements(false);
+  }
+}
+function checkLoginState() {
+  FB.getLoginStatus(function(response) {
+    statusChangeCallback(response);
+  });
+}
+function testAPI(){
+  FB.api('/me?fields=name,email,birthday,location', function(response){
+    if(respnse && !response.error){
+      //console.log(response);
+      buildProfile(response);
+    }
+    FB.api('/me/feed', function(response){
+      if(respnse && !response.error){
+      //console.log(response);
+      buildFeed(response);
+    }
+    });
+  })
+}
+function buildProfile(user){
+  let profile = `
+    <h3>${user.name}</h3>
+    <ul class="list-group">
+      <li class="list-group-item>User ID: ${user.id}</li>
+      <li class="list-group-item>Email ID: ${user.email}</li>
+      <li class="list-group-item>Birthday ID: ${user.birthday}</li>
+      <li class="list-group-item>Location ID: ${user.location.name}</li>
+    </ul>
+  `;
+  document.getElementById('profile').innerHTML = profile;
+}
+function buildFeed(feed){
+  let output = '<h3>Latest Posts</h3>';
+  for(let i in feed.data){
+    if(feed.data[i].message){
+      outout += `
+        <div class="well">
+          ${feed.data[i].message}
+        </div>
+      `;
+    }
+  }
+  document.getElementById('feed').innerHTML = output;
+}
+function setElements(isLoggedIn){
+  if(isLoggedIn){
+    document.getElementById('logout').style.display = 'block';
+    document.getElementById('profile').style.display = 'block';
+    document.getElementById('feed').style.display = 'block';
+    document.getElementById('fb-btn').style.display = 'none';
+    document.getElementById('heading').style.display = 'none';
+  } else {
+    document.getElementById('logout').style.display = 'none';
+    document.getElementById('profile').style.display = 'none';
+    document.getElementById('feed').style.display = 'none';
+    document.getElementById('fb-btn').style.display = 'block';
+    document.getElementById('heading').style.display = 'block';
+  }
+}
+function logout(){
+  FB.logout(function(response){
+    setElements(false);
+  });
+}
